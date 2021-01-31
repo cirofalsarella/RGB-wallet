722b55c7ec75185fac32c3256595e7644f0ed976
cirofalsarella <cirofalsarella@gmail.com>
2021-01-31 12:54:06 -0300

    arrumei os arquivos

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Admin from './pages/Admin'
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+  } from "react-router-dom";
+import react from "react";
+
+function App() {
+  return (
+    <Admin />
+  );
+}
+
+export default App;
---
+++ src/pages/Admin/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../../assets/styles.css'
+
+export default function Admin() {
+    return (
+        <h1 class="Cp">Página Admin</h1>
+    )
+}
