1297a37de08bd6840f18db8a8465d453caea2a0f
cirofalsarella <cirofalsarella@gmail.com>
2021-02-13 01:40:33 -0300

    front sem css login

--- Front/src/pages/Login/index.js
+++ Front/src/pages/Login/index.js
@@ -1,11 +1,52 @@
-import React from 'react';
-import './styles.css';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
+import RGB_sol from '../../assets/RGB_sol.png'
+import ICMC_logo from '../../assets/ICMC_logo.png'
+
+import './styles.css';
+
 export default function Login() {
+
+    const [user_name, setUserName] = useState('');
+    const [password, setPassword] = useState('');
+
+
+    function loginUser(){
+        console.log(user_name);
+    }
+
+    function loginAdmin(){
+        console.log(password);
+    }
+
     return (
         <div>
-            Login
+            <div className="main-content">
+
+                <div className="">Carteira RGB</div>
+                
+                <ul className="login-form">
+                    <li>
+                        <input 
+                            className="text-box"
+                            value={ user_name }
+                            onChange={ e => setUserName(e.target.value) } />
+                        <button onClick={ loginUser } >Login como Usuário</button>
+                    </li>
+                    <li>
+                        <input 
+                            className="text-box"
+                            value={ password }
+                            onChange={ e => setPassword(e.target.value) } />
+                        <button onClick={ loginAdmin } >Login como Admin</button>
+                    </li>
+                </ul>
+            
+            </div>
+
+            <div className="side-bar"></div>
+
         </div>
     )
 }
